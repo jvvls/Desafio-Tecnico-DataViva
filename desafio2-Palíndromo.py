15923d8d8d2374e377c2483db924767761b6c86e
João Victor Vial <joaovictorvial@gmail.com>
2026-02-04 14:26:55 -0300

    Desafio 2: Verificador de Palíndromo feito

---
+++ desafio2-Palíndromo.py
@@ -0,0 +1,16 @@
+def invert_string(word):
+    inverted_word = ""
+
+    for char in word:
+        inverted_word = char + inverted_word  # Pega o charactere atual e poe primeiro num novo string 
+
+    return inverted_word
+
+def palindromo_checker(word):
+    return  word == invert_string(word)
+    
+def main():
+    word = input("Insira palavra:")
+    print(palindromo_checker(word))
+
+main()
